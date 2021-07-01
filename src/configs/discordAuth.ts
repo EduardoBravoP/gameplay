b0c69c034750c97fb6aea7b9bca887522b2960fa
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-01 15:04:03 -0300

    add configs for discord authorization

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,5 @@
+export const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-bc5509d3-73fd-4f9e-bbdb-7d4782aacaaa';
+export const SCOPE = 'identify%20email%20connections%20guilds';
+export const RESPONSE_TYPE = 'token';
+export const CLIENT_ID = '860202846950916166';
+export const CDN_IMAGE = 'https://cdn.discordapp.com'
