8e1cc54e3835890a1532fdd67c46c43583624cca
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-01 15:02:27 -0300

    add ignore logs

--- App.tsx
+++ App.tsx
@@ -4,9 +4,11 @@ import AppLoading from 'expo-app-loading'
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 
-import { StatusBar } from 'react-native';
-import { Background } from './src/components/Background';
+import { LogBox, StatusBar } from 'react-native';
 import { Routes } from './src/routes/index.routes';
+import { AuthProvider } from './src/hooks/useAuth';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.'])
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +30,9 @@ export default function App() {
         translucent
       />
 
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </>
   );
 }
