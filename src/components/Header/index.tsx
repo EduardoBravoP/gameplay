c7404fc1dc1cc50afc9759706f9da056fff1034b
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-01 15:03:15 -0300

    adjust the space in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -33,10 +33,12 @@ function Header({ title, action }: HeaderProps) {
       </Text>
 
       {
-        action &&
+        action ?
         <View>
           { action }
         </View>
+        :
+        <View style={{ width: 24 }} />
       }
     </LinearGradient>
   );
